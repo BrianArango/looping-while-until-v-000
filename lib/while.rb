4d68fd0867742262cd6cda48e3e81333d15af00d
Brian Arango <brianvictus@gmail.com>
2020-07-20 01:35:37 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -5,36 +5,3 @@ def using_while
     levitation_force += 1
   end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=begin
-def using_while
-  levitation_force = 6
-	while levitation_force < 10
-  levitation_force += 1 
-   puts "Wingardium Leviosa"
-  end
-end
-=end
